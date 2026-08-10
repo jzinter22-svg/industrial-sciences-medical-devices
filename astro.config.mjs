0c699e7c9b8e9fb9ff787831e2c5d9f1f77c8f0a
jzinter22-svg <jzinter22@gmail.com>
2026-08-11 01:33:08 +0300

    chore: configure astro project

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://jzinter22-svg.github.io/industrial-sciences-medical-devices/',
+  base: '/industrial-sciences-medical-devices',
+});
